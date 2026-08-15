cb5bbc6a70391df904c9be919d43a4d50c601ac6
Alexander Svensson <alexandersvensson99@outlook.com>
2026-08-15 04:52:39 +0200

    Set PkgLift version to 0.2.0

--- Sources/PkgLiftCore/Version.swift
+++ Sources/PkgLiftCore/Version.swift
@@ -2,7 +2,7 @@
 // Core version constant for PkgLift.
 
 /// The current version of PkgLift.
-public let pkgLiftVersion = "0.1.2"
+public let pkgLiftVersion = "0.2.0"
 
 /// The product tagline.
 public let pkgLiftTagline = "Modernize Apple dependencies safely."
