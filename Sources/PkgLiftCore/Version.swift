df76fa969379d510aa5583b18c65fdf0af749216
Alexander Svensson <alexandersvensson99@outlook.com>
2026-08-15 13:06:30 +0200

    Add social preview for the Pages site

--- Sources/PkgLiftCore/Version.swift
+++
@@ -1,8 +0,0 @@
-// PkgLiftCore/Version.swift
-// Core version constant for PkgLift.
-
-/// The current version of PkgLift.
-public let pkgLiftVersion = "0.2.0"
-
-/// The product tagline.
-public let pkgLiftTagline = "Modernize Apple dependencies safely."
